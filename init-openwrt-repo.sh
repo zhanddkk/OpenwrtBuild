ac4b291e76fa7dcc895ad63d17a8f5cee20031b5
lay zhan <lei.zhan@csdental.com>
2022-01-20 12:33:27 +0800

    update scripts

--- init-openwrt-repo.sh
+++ init-openwrt-repo.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
-DIR=openwrt
+set -e
+
+[ $1 ] && COMMIT_ID=$1 || COMMIT_ID=master
+[ $2 ] && DIR=$2 || DIR=openwrt-build
+
 rm -rf ${DIR}
 mkdir -p ${DIR}
 cd ${DIR}
+
 git init .
 git remote add origin https://github.com/openwrt/openwrt.git
-git fetch --no-tags --prune --progress --no-recurse-submodules --depth=1 origin master
-git checkout --progress --force -B master refs/remotes/origin/master
+
+if [ "${COMMIT_ID}" = "master" ]; then
+	# master branch
+	echo "default master branch..."
+	git -c protocol.version=2 fetch --no-tags --prune --progress --no-recurse-submodules --depth=1 origin ${COMMIT_ID}
+	git checkout --progress --force -B build refs/remotes/origin/master
+else
+	# use commit id
+	echo "commit-id: ${COMMIT_ID}"
+	git -c protocol.version=2 fetch --no-tags --prune --progress --no-recurse-submodules --depth=1 origin +${COMMIT_ID}:refs/remotes/origin/master
+	git checkout --progress --force -B build ${COMMIT_ID}
+fi
+git log -1 --format='%H'
+exit $?
+
